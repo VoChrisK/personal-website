50c92bcab5add5e5aa7d563f6c5c188283642bcc
CurrySoup <vo.chris.k@gmail.com>
2020-02-24 10:42:17 -0800

    Fix scrolling bug

--- src/components/hero.js
+++ src/components/hero.js
@@ -3,9 +3,9 @@ import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 const Hero = () => {
 const one = <h3 className="subtitle first" style={{"transitionDelay":"1300ms"}}>Hello, my name is</h3>;
-    const two = <h1 className="title second" style={{ "transitionDelay": "2050ms" }}>Chris Vo</h1>;
-    const three = <h1 className="title third" style={{ "transitionDelay": "2800ms" }}>Mathematician turned Software Engineer</h1>;
-    const four = <p className="description fourth" style={{ "transitionDelay": "3550ms" }}>I'm a software engineer based in San Jose, CA specializing in building websites and applications with a focus in intuitive design and visual presentation</p>;
+const two = <h1 className="title second" style={{ "transitionDelay": "1500ms" }}>Chris Vo</h1>;
+const three = <h1 className="title third" style={{ "transitionDelay": "1700ms" }}>Mathematician turned Software Engineer</h1>;
+const four = <p className="description fourth" style={{ "transitionDelay": "1900ms" }}>I'm a software engineer based in San Jose, CA specializing in building websites and applications with a focus in intuitive design and visual presentation</p>;
 
     let list = [one, two, three, four]
 
@@ -15,7 +15,7 @@ const one = <h3 className="subtitle first" style={{"transitionDelay":"1300ms"}}>
                 {
                     list.map((ele, idx) => {
                             return (
-                                <CSSTransition key={idx} appear classNames="fade-up" timeout={1300 + (750 * (idx + 1))}>
+                                <CSSTransition key={idx} classNames="fade-up" appear timeout={1300 + (200 * (idx + 1))} exit={false}>
                                     {ele}
                                 </CSSTransition> 
                             )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Hero from './../components/hero';
 import Navbar from './../components/navbar';
 import Info from './../components/info';
@@ -8,35 +8,41 @@ import Skills from './../components/skills';
 import Contact from './../components/contact';
 import Footer from './../components/footer';
 
-let currentScroll = 0;
-// setTimeout(() => {
-//     document.getElementsByClassName("info-navbar")[0].classList.add("show-up");
-// }, 800)
-
-document.addEventListener("scroll", event => {
-    if (document.documentElement.scrollTop === 0 || document.documentElement.scrollTop > 5940) {
-        document.getElementsByClassName("navbar")[0].classList.remove("show-down");
-        document.getElementsByClassName("info-navbar")[0].classList.remove("hide-down");
-        document.getElementsByClassName("navbar")[0].classList.add("show-down");
-        document.getElementsByClassName("info-navbar")[0].classList.add("show-up");
-        currentScroll = document.documentElement.scrollTop;
-    }
-    else if (document.documentElement.scrollTop > currentScroll) {
-        document.getElementsByClassName("navbar")[0].classList.remove("show-down");
-        document.getElementsByClassName("navbar")[0].classList.add("hide-up");
-        document.getElementsByClassName("info-navbar")[0].classList.remove("hide-down");
-        document.getElementsByClassName("info-navbar")[0].classList.add("show-up");
-        currentScroll = document.documentElement.scrollTop;
-    } else if (document.documentElement.scrollTop < currentScroll) {
-        document.getElementsByClassName("navbar")[0].classList.remove("hide-up");
-        document.getElementsByClassName("navbar")[0].classList.add("show-down");
-        document.getElementsByClassName("info-navbar")[0].classList.remove("show-up");
-        document.getElementsByClassName("info-navbar")[0].classList.add("hide-down");
-        currentScroll = document.documentElement.scrollTop;        
-    } 
-})
-
 const Index = () => {
+    useEffect(() => {
+        if(typeof window === 'undefined' || typeof document === 'undefined') {
+            return;
+        }
+
+        let currentScroll = 0;
+        window.setTimeout(() => {
+            document.getElementsByClassName("info-navbar")[0].classList.add("show-up");
+        }, 800)
+
+        document.addEventListener("scroll", event => {
+            if (document.documentElement.scrollTop === 0 || document.documentElement.scrollTop > 5940) {
+                document.getElementsByClassName("navbar")[0].classList.remove("show-down");
+                document.getElementsByClassName("info-navbar")[0].classList.remove("hide-down");
+                document.getElementsByClassName("navbar")[0].classList.add("show-down");
+                document.getElementsByClassName("info-navbar")[0].classList.add("show-up");
+                currentScroll = document.documentElement.scrollTop;
+            }
+            else if (document.documentElement.scrollTop > currentScroll) {
+                document.getElementsByClassName("navbar")[0].classList.remove("show-down");
+                document.getElementsByClassName("navbar")[0].classList.add("hide-up");
+                document.getElementsByClassName("info-navbar")[0].classList.remove("hide-down");
+                document.getElementsByClassName("info-navbar")[0].classList.add("show-up");
+                currentScroll = document.documentElement.scrollTop;
+            } else if (document.documentElement.scrollTop < currentScroll) {
+                document.getElementsByClassName("navbar")[0].classList.remove("hide-up");
+                document.getElementsByClassName("navbar")[0].classList.add("show-down");
+                document.getElementsByClassName("info-navbar")[0].classList.remove("show-up");
+                document.getElementsByClassName("info-navbar")[0].classList.add("hide-down");
+                currentScroll = document.documentElement.scrollTop;
+            }
+        })
+    })
+
     return (
         <div id="root">
             <Navbar />
