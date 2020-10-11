5d08cc7515c2bc4fa370469a7269f1284a09334f
VoChrisK <vo.chris.k@gmail.com>
2020-10-11 12:35:20 -0700

    add console.log to check error on prod

--- src/components/projects/main_projects.js
+++ src/components/projects/main_projects.js
@@ -4,6 +4,8 @@ import { projectImagePaths } from '../../data/paths'
 import { MainProject } from './main_project';
 
 export const MainProjects = () => {
+    console.log(typeof myMainProjects)
+
     return (
         <section id="projects" className="projects section">
         <h1 className="header" data-sal='slide-up' data-sal-easing="ease" data-sal-duration="1000">Portfolio</h1>
