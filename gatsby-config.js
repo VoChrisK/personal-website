0e514110f5741507d38de11a8169e2b22c367865
VoChrisK <vo.chris.k@gmail.com>
2020-12-26 14:43:10 -0800

    update skills

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,12 @@
 
 module.exports = {
   siteMetadata: {
-    title: `Chris Vo | Software Engineer`
+    title: `Chris Vo | Software Engineer`,
+    name: `Chris Vo`,
+    siteURL: `https://chrisvo.dev/`,
+    linkedInURL: `https://www.linkedin.com/in/chris-vo-/`,
+    githubURL: `https://github.com/VoChrisK`,
+    angellistURL: `https://angel.co/chris-vo-3`
   },
   plugins: [
     `gatsby-plugin-scroll-reveal`,
--- src/data/paths.js
+++ src/data/paths.js
@@ -6,18 +6,18 @@ export const projectImagePaths = [
 
 export const skillIconPaths = [
     require("../images/typescript-logo.jpeg"),
-    require("../images/javascript-logo.png"),
+    require("../images/sass-logo.png"),
+    require("../images/html-logo.png"),
     require("../images/angular-logo.png"),
     require("../images/ngrx-logo.svg"),
     require("../images/jest-logo.png"),
-    require("../images/sass-logo.png"),
-    require("../images/html-logo.png"),
-    require("../images/react-logo.png"),
+    require("../images/rxjs-logo.png"),
+    require("../images/graphql-logo.png"),
     require("../images/nodejs-logo.png"),
+    require("../images/nestjs-logo.svg"),
     require("../images/git-logo.png"),
-    require("../images/cypress-logo.png"),
+    require("../images/react-logo.png"),
     require("../images/java-logo.png"),
     require("../images/mysql-logo.png"),
-    require("../images/rails-logo.png"),
-    require("../images/webpack-logo.png")
+    require("../images/rails-logo.png")
 ]
